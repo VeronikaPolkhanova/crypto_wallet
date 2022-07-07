3580ff06d8962f5ab5f2a86b7595d7497fd70633
VeronikaPolkhanova <halky.boom>
2022-07-07 21:18:36 +0300

    fix double crypto

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,14 +6,14 @@ const defaultState = {
 }
 
 const reducer = (state = defaultState, action) => {
+    console.log(state.wallet);
     switch (action.type) {
-
         case ADD_ALL_API:
             return { ...state, crypto: [...action.payload] }
 
         case ADD_CRYPTO:
-            if (state.wallet.id === action.payload.data.id) {
-                return { ...state, wallet: [...state.wallet, action.payload] }
+            if (state.wallet.length !== 0 && state.wallet.find(it => it.data.id === action.payload.data.id)) {
+                return { ...state, wallet: state.wallet.map(it => it.data.id === action.payload.data.id ? { ...it, count: +it.count + +action.payload.count } : it) }
             }
             else {
                 return { ...state, wallet: [...state.wallet, action.payload] }
