cb10207131f1f369053e3be342f6273389cea8c2
VeronikaPolkhanova <halky.boom>
2022-07-08 02:33:01 +0300

    added pagination

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -4,15 +4,15 @@ import { useNavigate } from 'react-router-dom';
 
 import { IoMdAddCircleOutline } from 'react-icons/io';
 import Numeral from 'numeral';
-
-
+import { Table, Header, HeaderCell, HeaderRow, Body, Row, Cell } from '@table-library/react-table-library/table';
+import { usePagination } from '@table-library/react-table-library/pagination';
 
 import { SetModalAction } from '../../store/actions';
 
 import './table.scss';
 import '../../design-tokens/modal.scss';
 
-function Table({ crypto }) {
+function Tableb({ crypto }) {
     const navigate = useNavigate();
     const handleOnClick = useCallback((it) => navigate(`/${it.id}`, { replace: false, state: it }), [navigate]);
 
@@ -21,43 +21,89 @@ function Table({ crypto }) {
         dispatch(SetModalAction(stateModal))
     }
 
+    const data = { nodes: crypto };
+    const pagination = usePagination(data, {
+        state: {
+            page: 0,
+            size: 15,
+        },
+    });
+
     return (
-        <table className="table">
-            <thead>
-                <tr className="table-header">
-                    <th>Rank</th>
-                    <th>Name</th>
-                    <th>Price</th>
-                    <th>Market cap</th>
-                    <th>VWAP(24hr)</th>
-                    <th>Supply</th>
-                    <th>Change(24hr)</th>
-                    <th>Volume(24hr)</th>
-                </tr>
-            </thead>
-            <tbody>
-                {crypto.map(it =>
-                    <tr className="table-row" key={it.id} onClick={() => handleOnClick(it)} >
-                        <td>{it.rank}</td>
-                        <td>{`${it.name} ${it.symbol}`}</td>
-                        <td>{Numeral(it.priceUsd).format('$00,00.00')}</td>
-                        <td>{Numeral(it.marketCapUsd).format('($0.00a)')}</td>
-                        <td>{Numeral(it.vwap24Hr).format('$00,00.00')}</td>
-                        <td>{Numeral(it.supply).format('($0.00a)')}</td>
-                        <td style={{ color: `${+it.changePercent24Hr < 0 ? 'red' : 'green'}` }}>{Numeral(it.changePercent24Hr).format('0.00')}%</td>
-                        <td className="td-whith-button">
-                            {Numeral(it.volumeUsd24Hr).format('($0.00a)')}
-                            <span className="add-crypto-button" onClick={(event) => {
-                                setModal({ isOpen: true, payload: { ...it } });
-                                event.stopPropagation()
-                            }} >
-                                <IoMdAddCircleOutline />
-                            </span>
-                        </td>
-                    </tr>
-                )}
-            </tbody>
-        </table>
+        <>
+            <Table data={data} pagination={pagination} className="table">{(tableList) =>
+                <>
+                    <Header>
+                        <HeaderRow className="table-header">
+                            <HeaderCell>Rank</HeaderCell>
+                            <HeaderCell>Name</HeaderCell>
+                            <HeaderCell>Price</HeaderCell>
+                            <HeaderCell>Market cap</HeaderCell>
+                            <HeaderCell>VWAP(24hr)</HeaderCell>
+                            <HeaderCell>Supply</HeaderCell>
+                            <HeaderCell>Change(24hr)</HeaderCell>
+                            <HeaderCell>Volume(24hr)</HeaderCell>
+                        </HeaderRow>
+                    </Header>
+                    <Body>
+                        {tableList.map((item) => (
+                            <Row className="table-row" key={item.id} item={item} key={item.id} onClick={() => handleOnClick(item)}>
+                                <Cell>{item.rank}</Cell>
+                                <Cell>{`${item.name} ${item.symbol}`}</Cell>
+                                <Cell>{Numeral(item.priceUsd).format('$00,00.00')}</Cell>
+                                <Cell>{Numeral(item.marketCapUsd).format('($0.00a)')}</Cell>
+                                <Cell>{Numeral(item.vwap24Hr).format('$00,00.00')}</Cell>
+                                <Cell>{Numeral(item.supply).format('($0.00a)')}</Cell>
+                                <Cell style={{ color: `${+item.changePercent24Hr < 0 ? 'red' : 'green'}` }}>{Numeral(item.changePercent24Hr).format('0.00')}%</Cell>
+                                <Cell>
+                                    <div style={{
+                                        display: "flex",
+                                        justifyContent: "space-between",
+                                        alignItems: "center"
+                                    }}>
+                                        <p style={{ margin: "0" }}>{Numeral(item.volumeUsd24Hr).format('($0.00a)')}</p>
+                                        <span className="add-crypto-button" onClick={(event) => {
+                                            setModal({ isOpen: true, payload: { ...item } });
+                                            event.stopPropagation()
+                                        }} >
+                                            <IoMdAddCircleOutline />
+                                        </span>
+                                    </div>
+                                </Cell>
+                            </Row>
+                        ))}
+                    </Body>
+                </>
+            }
+            </Table>
+            <div
+                style={{ display: 'flex', justifyContent: 'space-between', width:'80%', margin: '10px 10%' }}
+            >
+                <span>
+                    Total Pages: {pagination.state.getTotalPages(data.nodes)}
+                </span>
+
+                <span>
+                    Page:{' '}
+                    {pagination.state.getPages(data.nodes).map((_, index) => (
+                        <button
+                            key={index}
+                            type="button"
+                            style={{
+                                fontWeight:
+                                    pagination.state.page === index
+                                        ? 'bold'
+                                        : 'normal',
+                            }}
+                            onClick={() => pagination.fns.onSetPage(index)}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
+                </span>
+            </div>
+        </>
+
     )
 }
-export default Table;
+export default Tableb;
